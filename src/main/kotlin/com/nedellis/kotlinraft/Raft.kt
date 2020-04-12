3465931a79f4bc368c2c61391ea707bd119d16b6
Ned Ellis <epe2@illinois.edu>
2020-04-11 22:06:38 -0500

    Use future to coroutine tranformation and add some more event loop logic

--- src/main/kotlin/com/nedellis/kotlinraft/Raft.kt
+++ src/main/kotlin/com/nedellis/kotlinraft/Raft.kt
@@ -2,19 +2,46 @@ package com.nedellis.kotlinraft
 
 import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.guava.await
 import kotlinx.coroutines.launch
 
 class Raft(val port: Int, val discoveryPort: Int) {
-    val discoveryChannel = ManagedChannelBuilder.forAddress("localhost", discoveryPort)
+    private val discoveryChannel = ManagedChannelBuilder.forAddress("localhost", discoveryPort)
         .usePlaintext()
         .build()
-    val discoveryStub = DiscoveryGrpc.newFutureStub(discoveryChannel)
-    var clients = mutableSetOf<Int>()
+    private val discoveryStub = DiscoveryGrpc.newFutureStub(discoveryChannel)
+    private var clients = mutableSetOf<Int>()
+    private val messageChannel = Channel<Int>(Channel.UNLIMITED)
 
-    fun run() = GlobalScope.launch {
+    val table = mutableMapOf<String, String>()
+
+    private suspend fun registerAndGetClients(): MutableSet<Int> {
         val request = RegisterNewClientRequest.newBuilder().setPort(port).build()
         val response = discoveryStub.registerNewClient(request).await()
-        clients = response.clientPortsList.toMutableSet()
+        return response.clientPortsList.toMutableSet()
+    }
+
+    // Use channel to talk between thread and coroutines
+    // Make an actor system to coordinate, similar to event loop
+
+    fun run() = GlobalScope.launch {
+        clients = registerAndGetClients()
+
+        // Debug coroutine
+        launch {
+            while (true) {
+                delay(1000L)
+                println("Clients of $port: $clients")
+            }
+        }
+
+        // Event loop coroutine, reads from GRPC received message channel
+        launch {
+            while (true) {
+                val message = messageChannel.receive()
+            }
+        }
     }
 }
