a0c429de5194090e7d44f1ed2dd4645c4f1c94c3
Ned Ellis <epe2@illinois.edu>
2020-04-12 15:10:52 -0500

    More actor patterns for raft

--- src/main/kotlin/com/nedellis/kotlinraft/Raft.kt
+++ src/main/kotlin/com/nedellis/kotlinraft/Raft.kt
@@ -1,9 +1,10 @@
 package com.nedellis.kotlinraft
 
 import io.grpc.ManagedChannelBuilder
+import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.guava.await
 import kotlinx.coroutines.launch
 
@@ -13,8 +14,11 @@ class Raft(val port: Int, val discoveryPort: Int) {
         .build()
     private val discoveryStub = DiscoveryGrpc.newFutureStub(discoveryChannel)
     private var clients = mutableSetOf<Int>()
-    private val messageChannel = Channel<Int>(Channel.UNLIMITED)
-
+    private val messageChannel = Channel<RaftMessage>(Channel.UNLIMITED)
+    private val server = ServerBuilder.forPort(port)
+        .addService(RaftImpl(messageChannel))
+        .build()
+    private val state = RaftState()
     val table = mutableMapOf<String, String>()
 
     private suspend fun registerAndGetClients(): MutableSet<Int> {
@@ -23,25 +27,60 @@ class Raft(val port: Int, val discoveryPort: Int) {
         return response.clientPortsList.toMutableSet()
     }
 
-    // Use channel to talk between thread and coroutines
-    // Make an actor system to coordinate, similar to event loop
-
     fun run() = GlobalScope.launch {
         clients = registerAndGetClients()
 
-        // Debug coroutine
-        launch {
-            while (true) {
-                delay(1000L)
-                println("Clients of $port: $clients")
-            }
-        }
+        Thread { server.start().awaitTermination() }.start()
 
         // Event loop coroutine, reads from GRPC received message channel
         launch {
             while (true) {
                 val message = messageChannel.receive()
+                when (message) {
+                    is RaftMessage.AppendEntries -> appendEntries(message)
+                    is RaftMessage.RequestVote -> requestVote(message)
+                }
             }
+        }
+    }
+
+    private fun appendEntries(ctx: RaftMessage.AppendEntries) {
+
+    }
+
+    private fun requestVote(ctx: RaftMessage.RequestVote) {
+
+    }
+
+    internal sealed class RaftMessage {
+        data class AppendEntries(val req: AppendEntriesRequest, val res: StreamObserver<AppendEntriesResponse>) :
+            RaftMessage()
+
+        data class RequestVote(val req: RequestVoteRequest, val res: StreamObserver<RequestVoteResponse>) :
+            RaftMessage()
+    }
+
+    internal data class RaftState(
+        var currentTerm: Int = 0,
+        var votedFor: Int = 0,
+        var log: List<Int> = emptyList(),
+        var commitIndex: Int = 0,
+        var lastApplied: Int = 0,
+        var nextIndex: MutableMap<Int, Int> = mutableMapOf(),
+        var matchIndex: MutableMap<Int, Int> = mutableMapOf()
+    )
+
+    // Runs the GRPC server. When a message is received, add to message channel and return immediately
+    internal class RaftImpl(val messageChannel: Channel<RaftMessage>) : RaftGrpc.RaftImplBase() {
+        override fun appendEntries(
+            request: AppendEntriesRequest?,
+            responseObserver: StreamObserver<AppendEntriesResponse>?
+        ) {
+            messageChannel.offer(RaftMessage.AppendEntries(request!!, responseObserver!!))
+        }
+
+        override fun requestVote(request: RequestVoteRequest?, responseObserver: StreamObserver<RequestVoteResponse>?) {
+            messageChannel.offer(RaftMessage.RequestVote(request!!, responseObserver!!))
         }
     }
 }
