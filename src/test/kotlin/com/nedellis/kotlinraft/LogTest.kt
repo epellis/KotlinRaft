b59a5ec0a8ba09e2ac3f2cfa0d36a10b553fc5a5
Ned Ellis <epe2@illinois.edu>
2020-05-03 17:35:38 -0500

    More tests

--- src/test/kotlin/com/nedellis/kotlinraft/LogTest.kt
+++ src/test/kotlin/com/nedellis/kotlinraft/LogTest.kt
@@ -21,4 +21,38 @@ class LogTest : StringSpec({
             .setSuccess(true)
             .build()
     }
+
+    "single log refresh" {
+        val req = mockk<AppendRequest>()
+        val entry = mockk<Entry>()
+
+        every { req.term } returns 0
+        every { req.leaderCommit } returns 0
+        every { req.prevLogIndex } returns 0
+        every { req.entriesList } returns listOf(entry)
+
+        val log = Log()
+
+        log.append(req) shouldBe AppendResponse.newBuilder()
+            .setTerm(0)
+            .setSuccess(true)
+            .build()
+    }
+
+    "many log refresh" {
+        val req = mockk<AppendRequest>()
+        val entry = mockk<Entry>()
+
+        every { req.term } returns 0
+        every { req.leaderCommit } returns 0
+        every { req.prevLogIndex } returns 0
+        every { req.entriesList } returns listOf(entry, entry, entry)
+
+        val log = Log()
+
+        log.append(req) shouldBe AppendResponse.newBuilder()
+            .setTerm(0)
+            .setSuccess(true)
+            .build()
+    }
 })
